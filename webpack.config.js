27203d7e0960e66c3ef88a2a3c445841f561e21c
bzraa <ubazraa3@gmail.com>
2022-01-30 00:41:09 +0800

    docs folder created for gitpage

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,32 @@ module.exports = {
     mode: "production",
     entry: "./src/js/index.js",
     output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
+        filename: "js/main.js",
+        path: path.resolve(__dirname, "docs")
     },
     devServer: {
-        static: "./dist",
+        static: "./docs",
     },
     plugins: [
         new HtmlWebpackPlugin({
             filename: "index.html",
             template: "src/index.html"
         })
-    ]
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ]
+              }
+            }
+          }
+        ]
+      }
 }
