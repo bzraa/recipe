9db843ec36b563e4e5a702fa37e469a1ab0d709b
bzraa <ubazraa3@gmail.com>
2022-01-30 01:03:02 +0800

    search controller and state added

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,20 @@
 require("@babel/polyfill");
 import Search from './model/Search';
 
-let search = new Search('pasta');
-search.doSearch().then(r => console.log(r));
+const state = {};
+
+const controlSearch = async () => {
+    const query = 'pizza';
+    if(query) {
+        state.search = new Search(query);
+
+        await state.search.doSearch();
+
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
